f18e0b84aceabd83c71007c5148f5902bbddca06
Vincent <vincent.devilers@gmail.com>
2025-11-05 19:47:11 +0100

    remove wrong import

--- shared/index.ts
+++ shared/index.ts
@@ -2,11 +2,9 @@ export * from "./entity/school_class";
 export * from "./entity/lesson";
 export * from "./entity/subject";
 export * from "./input/school_class_create";
-export * from "./input/subject_create";
 export {
   school_class_patch_input,
   school_class_update_input,
   type SchoolClassPatchInput,
   type SchoolClassUpdateInput,
 } from "./input/school_class_create";
-
