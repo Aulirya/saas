ed015d46186e208746610c49b00e244fdce78786
Vincent <vincent.devilers@gmail.com>
2025-11-20 21:47:57 +0100

    fix(lessons): update model, entity and mapper since our datbase has changed

--- backend/src/repository/mapper/lesson.ts
+++ backend/src/repository/mapper/lesson.ts
@@ -5,13 +5,15 @@ export namespace LessonMapper {
   export function fromModel(model: LessonModel): Lesson {
     return {
       id: model.id.toString(),
-      title: model.title,
-      date: model.date,
-      duration_minutes: model.duration_minutes,
-      average_duration: model.average_duration,
-      status: model.status,
       class_id: model.class_id.toString(),
       subject_id: model.subject_id.toString(),
+      description: model.description,
+      label: model.label,
+      start_at: model.start_at ? model.start_at.toString() : null,
+      end_at: model.end_at ? model.end_at.toString() : null,
+      comments: model.comments.map((comment) => comment.title),
+      created_at: model.created_at.toString(),
+      updated_at: model.updated_at.toString(),
     };
   }
 }
