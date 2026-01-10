22f8e3fc7395f449eb0c71b768daa0ecbd4e536d
SophieG <sophie.gillard@esnah.com>
2026-01-10 12:04:08 +0100

    DTO updated with country

--- backend/src/webhooks/dto.ts
+++ backend/src/webhooks/dto.ts
@@ -1,11 +1,14 @@
 export interface ClerkUserWebhookDto {
-  id: string;
-  email_addresses: Array<ClerkWebhookEmailDto>;
-  first_name: string | null;
-  last_name: string | null;
+    id: string;
+    public_metadata: {
+        country: "FR" | "BE";
+    };
+    email_addresses: Array<ClerkWebhookEmailDto>;
+    first_name: string | null;
+    last_name: string | null;
 }
 
 interface ClerkWebhookEmailDto {
-  email_address: string;
-  id: string;
+    email_address: string;
+    id: string;
 }
